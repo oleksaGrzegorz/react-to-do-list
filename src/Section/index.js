4c0f5000290d18cc2958a1d1372b58913c8b646f
Grzegorz Oleksa <grzesieq92@gmail.com>
2022-04-13 20:42:59 +0200

    text formatted

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,7 +1,7 @@
 import "./style.css";
 
-const Section = ({title, body, extraHeaderContent}) => (
-<section className="section">
+const Section = ({ title, body, extraHeaderContent }) => (
+    <section className="section">
         <header className="section__header">
             <h2 className="section__title">{title}</h2>
             {extraHeaderContent}
