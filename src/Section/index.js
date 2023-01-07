3dc2a08edc1d81c90769c732634b77cc050d2fe0
Grzegorz Oleksa <grzesieq92@gmail.com>
2023-01-07 13:09:57 +0100

    styled to Section somponent added

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,16 +1,16 @@
-import "./style.css";
+import { MainSection, Title, Header, SectionBody } from "./styled";
 
 const Section = ({ title, body, extraHeaderContent }) => (
-    <section className="section">
-        <header className="section__header">
-            <h2 className="section__title">{title}</h2>
+    <MainSection>
+        <Header>
+            <Title>{title}</Title>
             {extraHeaderContent}
-        </header>
-        <div className="section__body">
+        </Header>
+        <SectionBody>
             {body}
-        </div>
+        </SectionBody>
 
-    </section>
+    </MainSection>
 );
 
 export default Section;
---
+++ src/Section/styled.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const MainSection = styled.section`
+  box-shadow: 2px -2px 5px 0px #dddddd;
+  background-color: white;
+  margin: 10px 0;
+`;
+
+export const Title = styled.h2`
+  padding: 20px;
+  font-size: 20px;
+  margin: 0;
+`;
+
+export const Header = styled.header`
+  border-bottom: 1px solid #ddd;
+  display: grid;
+  grid-template-columns: auto auto;
+  justify-content: space-between;
+  grid-gap: 20px;
+  align-items: center;
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+    grid-gap: 0;
+  }
+`;
+
+export const SectionBody = styled.div`
+  padding-bottom: 10px;
+`;
