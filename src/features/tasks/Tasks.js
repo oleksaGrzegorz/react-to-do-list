09e8d20d69cc7af246bfc0847009b91476629035
Grzegorz Oleksa <grzesieq92@gmail.com>
2023-03-13 22:35:32 +0100

    application split into features

---
+++ src/features/tasks/Tasks.js
@@ -0,0 +1,56 @@
+import Form from "./Form";
+import TasksList from "../../features/tasks/TasksList";
+import Buttons from "./Buttons";
+import Section from "../../common/Section";
+import Header from "../../common/Header";
+import Container from "../../common/Container";
+import { useState} from "react";
+import { useTasks } from "../../useTasks";
+
+function Tasks() {
+
+  const [hideDone, setHideDone] = useState(false);
+  const toggleHideDone = () => {
+    setHideDone((hideDone) => !hideDone);
+  };
+
+  const { 
+    tasks, 
+    removeTask, 
+    toggleTaskDone, 
+    setAllDone, 
+    addNewTask 
+  } = useTasks();
+
+  return (
+    <Container>
+      <Header title="Lista zadań" />
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form addNewTask={addNewTask} />}
+      />
+
+      <Section
+        title="Lista zadań"
+        body={
+          <TasksList
+            tasks={tasks}
+            hideDone={hideDone}
+            removeTask={removeTask}
+            toggleTaskDone={toggleTaskDone}
+          />
+        }
+        extraHeaderContent={
+          <Buttons
+            tasks={tasks}
+            hideDone={hideDone}
+            toggleHideDone={toggleHideDone}
+            setAllDone={setAllDone}
+          />
+        }
+      />
+    </Container>
+  );
+}
+
+export default Tasks;
--- src/features/tasks/TasksList/index.js
+++ src/features/tasks/TasksList/index.js
@@ -1,6 +1,6 @@
 import { List, Item, Content, RemoveButton, DoneButton } from "./styled";
 
-const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
+const TasksList = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
   <List>
     {tasks.map((task) => (
       <Item key={task.id} hidden={task.done && hideDone}>
@@ -23,4 +23,4 @@ const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
   </List>
 );
 
-export default Tasks;
+export default TasksList;
