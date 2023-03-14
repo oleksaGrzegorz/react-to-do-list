82c40455385d9c752963ce82fde70c7808a5b9cf
Grzegorz Oleksa <grzesieq92@gmail.com>
2023-03-14 14:59:02 +0100

    settAllDone - added with redux

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -13,18 +13,28 @@ const tasksSlice = createSlice({
     toggleHideDone: (state) => {
       state.hideDone = !state.hideDone;
     },
-    toggleTaskDone: (state, action) => {
-      const index = state.tasks.findIndex((task) => task.id === action.payload);
-      state.tasks[index].done = !state.tasks[index].done;
+    toggleTaskDone: ({ tasks }, { payload }) => {
+      const index = tasks.findIndex(({ id }) => id === payload);
+      tasks[index].done = !tasks[index].done;
     },
     removeTask: ({ tasks }, { payload }) => {
       const index = tasks.findIndex(({ id }) => id === payload);
       tasks.splice(index, 1);
     },
+    setAllDone: ({ tasks }) => {
+      for (const task of tasks) {
+        task.done = true;
+      }
+    },
   },
 });
 
-export const { addTask, toggleHideDone, toggleTaskDone, removeTask } =
-  tasksSlice.actions;
+export const {
+  addTask,
+  toggleHideDone,
+  toggleTaskDone,
+  removeTask,
+  setAllDone,
+} = tasksSlice.actions;
 export const selectTasks = (state) => state.tasks;
 export default tasksSlice.reducer;
