bff1353047900470173981f0a925c39f89baf805
Grzegorz Oleksa <grzesieq92@gmail.com>
2023-03-14 13:35:58 +0100

    hideDone - redux

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,18 +1,24 @@
 import { Button } from "./styled";
+import { useSelector, useDispatch } from "react-redux";
+import { selectTasks, toggleHideDone } from "../tasksSlice";
 
-const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
-  <div>
-    {tasks.length > 0 && (
-      <>
-        <Button onClick={toggleHideDone}>
-          {hideDone ? "Pokaż" : "Ukryj"} ukończone
-        </Button>
-        <Button onClick={setAllDone} disabled={tasks.every(({ done }) => done)}>
-          Ukończ wszystkie
-        </Button>
-      </>
-    )}
-  </div>
-);
+const Buttons = ({ setAllDone }) => {
+  const { tasks, hideDone } = useSelector(selectTasks);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      {tasks.length > 0 && (
+        <>
+          <Button onClick={() => dispatch(toggleHideDone())}>
+            {hideDone ? "Pokaż" : "Ukryj"} ukończone
+          </Button>
+          <Button onClick={setAllDone} disabled={tasks.every(({ done }) => done)}>
+            Ukończ wszystkie
+          </Button>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Buttons;
