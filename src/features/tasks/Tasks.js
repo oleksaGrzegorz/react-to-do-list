943b0c151a0723a5f2fa8870e5a5fae03eaee8b8
Grzegorz Oleksa <grzesieq92@gmail.com>
2023-03-14 14:59:59 +0100

    unnecessary code deleted

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -4,27 +4,17 @@ import Buttons from "./Buttons";
 import Section from "../../common/Section";
 import Header from "../../common/Header";
 import Container from "../../common/Container";
-import { useTasks } from "../../useTasks";
-import { useSelector } from "react-redux";
-import { selectTasks } from "./tasksSlice";
 
 function Tasks() {
-  const { tasks } = useSelector(selectTasks);
-
-  const { toggleTaskDone, setAllDone, addNewTask } = useTasks();
-
   return (
     <Container>
       <Header title="Lista zadań" />
-      <Section
-        title="Dodaj nowe zadanie"
-        body={<Form addNewTask={addNewTask} />}
-      />
+      <Section title="Dodaj nowe zadanie" body={<Form />} />
 
       <Section
         title="Lista zadań"
-        body={<TasksList toggleTaskDone={toggleTaskDone} />}
-        extraHeaderContent={<Buttons setAllDone={setAllDone} />}
+        body={<TasksList />}
+        extraHeaderContent={<Buttons />}
       />
     </Container>
   );
