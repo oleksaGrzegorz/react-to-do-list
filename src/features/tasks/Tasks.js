5100937e783c1e67941d9a8904d32d330036b770
Grzegorz Oleksa <grzesieq92@gmail.com>
2023-03-16 11:37:07 +0100

    exampleTasks in json added and fetchExampleTasks

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -4,12 +4,23 @@ import Buttons from "./Buttons";
 import Section from "../../common/Section";
 import Header from "../../common/Header";
 import Container from "../../common/Container";
+import { fetchExampleTasks } from "./tasksSlice";
+import { useDispatch } from "react-redux";
 
 function Tasks() {
+  const dispatch = useDispatch();
   return (
     <Container>
       <Header title="Lista zadań" />
-      <Section title="Dodaj nowe zadanie" body={<Form />} />
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form />}
+        extraHeaderContent={
+          <button onClick={() => dispatch(fetchExampleTasks())}>
+            Pobierz przykładowe zadania
+          </button>
+        }
+      />
 
       <Section
         title="Lista zadań"
