7f6b4376eb1b7f3991ea1b262e80f90f0eb7f4d2
Grzegorz Oleksa <grzesieq92@gmail.com>
2023-03-16 09:16:40 +0100

    React Router added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
 import React from "react";
-import  Tasks  from "./features/tasks/Tasks";
-const App = () => <Tasks />;
+import Tasks from "./features/tasks/Tasks";
+import Author from "./features/author/AuthorPage";
+import { Link, Switch, Route, HashRouter, Redirect } from "react-router-dom";
+const App = () => (
+  <HashRouter>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/tasks">Zadania</Link>
+        </li>
+        <li>
+          <Link to="/author">O autorze</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/tasks">
+          <Tasks />
+        </Route>
+        <Route path="/author">
+          <Author />
+        </Route>
+        <Route path="/">
+          <Redirect to="/tasks" />
+        </Route>
+      </Switch>
+    </nav>
+  </HashRouter>
+);
 
 export default App;
